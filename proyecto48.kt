ad6d826cbd554dc7b4eb776ff3d3118c20ee526c
Yulissa186 <yuli125blas@gmail.com>
2025-09-20 23:51:55 -0600

    Add files via upload

---
+++ proyecto48.kt
@@ -0,0 +1,11 @@
+fun main(parametro: Array<String>) {
+    var suma = 0
+    for(i in 1..10) {
+        print("Ingrese un valor:")
+        val valor = readln().toInt()
+        suma += valor
+    }
+    println("La suma de los valores ingresados es $suma")
+    val promedio = suma / 10
+    println("Su promedio es $promedio")
+}
