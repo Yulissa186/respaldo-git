69a5622bd6615ed49d88a7b91043c2bf80c4705f
Yulissa186 <yuli125blas@gmail.com>
2025-10-19 22:37:08 -0600

    Add files via upload

---
+++ proyecto58.kt
@@ -0,0 +1,23 @@
+fun main(parametro: Array<String>) {
+    var negativos = 0
+    var positivos = 0
+    var mult15 = 0
+    var sumapares = 0
+    for(i in 1..10) {
+        print("Ingrese valor:")
+        val valor = readln().toInt()
+        if (valor < 0)
+            negativos++
+        else
+            if (valor > 0)
+                positivos++
+        if ( valor % 15 == 0)
+            mult15++
+        if (valor % 2 == 0)
+            sumapares+=valor
+    }
+    println("Cantidad de valores negativos: $negativos")
+    println("Cantidad de valores positivos: $positivos")
+    println("Cantidad de valores múltiplos de 15: $mult15")
+    println("Suma de los valores pares: $sumapares")
+}
